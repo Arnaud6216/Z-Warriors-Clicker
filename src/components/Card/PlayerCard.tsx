27e1f3707a9a58e07076511e9fc7c0df25064896
Arnaud6216 <guevaer8@gmail.com>
2024-11-14 13:47:13 +0100

    adding super saiyen 2

--- src/components/Card/PlayerCard.tsx
+++ src/components/Card/PlayerCard.tsx
@@ -3,32 +3,31 @@ import { Context } from "../options/Context";
 import "./card.css";
 
 function Card() {
-    const context = useContext(Context);
+	const context = useContext(Context);
 
-    if (!context) {
-        // Si le context est `undefined`, vous pouvez retourner un message d'erreur ou un comportement par défaut
-        return <div>Error: Context is not available!</div>;
-    }
+	if (!context) {
+		return <div>Error: Context is not available!</div>;
+	}
 
-    const { gifSrc, count, setCount } = context;
+	const { gifSrc, count, setCount } = context;
 
-    const handleClickCount = () => {
-        setCount(count + 1);
-    };
+	const handleClickCount = () => {
+		setCount(count + 1);
+	};
 
-    return (
-        <div className="card-container">
-            <img
-                src={gifSrc[0]} // Affiche le GIF de Goku dans son état normal
-                alt="Goku"
-                width="390px"
-                height="220px"
-            />
-            <button className="power-button" type="button" onClick={handleClickCount}>
-                Power Level: {count}
-            </button>
-        </div>
-    );
+	return (
+		<div className="card-container">
+			<img
+				src={gifSrc[0]} // Affiche le GIF de Goku dans son état normal
+				alt="Goku"
+				width="390px"
+				height="220px"
+			/>
+			<button className="power-button" type="button" onClick={handleClickCount}>
+				Power Level: {count}
+			</button>
+		</div>
+	);
 }
 
 export default Card;
--- src/components/Spec/Spec.tsx
+++ src/components/Spec/Spec.tsx
@@ -53,21 +53,30 @@ function Spec() {
 		return () => clearInterval(interval);
 	}, [concentrationCount, concentrationIncrement, setCount]);
 
-	// Gestion du changement de gif lors du clic sur Super Saiyen
 	const handleClickSsj = () => {
 		if (count >= 50 && gif !== 1) {
-			// Vérifie si le gif n'est pas déjà en état 1
-			setGif(1); // Change le gif à 1 (transition en Super Saiyen)
+			setGif(1);
 
-			// Change le gif à 2 après 12 secondes (forme complète Super Saiyen)
 			setTimeout(() => {
 				setGif(2);
 			}, 10500);
 
-			// Décrémente le count pour le coût de la transformation
 			setCount(count - 50);
 		}
 		setSaiyenState(1);
+	};
+
+	const handleClickSsj2 = () => {
+		if (count >= 100 && gif !== 3) {
+			setGif(3);
+
+			setTimeout(() => {
+				setGif(4);
+			}, 3000);
+
+			setCount(count - 100);
+		}
+		setSaiyenState(2);
 	};
 
 	return (
@@ -84,7 +93,15 @@ function Spec() {
 						label="Super Saiyen"
 						isAvailable={count >= 50}
 						onClick={handleClickSsj}
-						className={count >= 50 ? "spec-option-available" : "spec-option"}
+						className="saiyan-option"
+					/>
+				)}
+				{count >= 100 && saiyenState === 1 && (
+					<Option
+						label="Super Saiyen 2"
+						isAvailable={count >= 50}
+						onClick={handleClickSsj2}
+						className="saiyan-option"
 					/>
 				)}
 			</ul>
