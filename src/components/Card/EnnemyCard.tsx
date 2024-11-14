5dd271fcc379fc4ad5b4d7248aa3cc43a2f51308
Arnaud6216 <guevaer8@gmail.com>
2024-11-14 12:25:25 +0100

    adding the ennemy content in the ennemy card

--- src/components/Card/EnnemyCard.tsx
+++ src/components/Card/EnnemyCard.tsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+
+const ennemyList = [
+	{
+		name: "Vegeta",
+		life: 10,
+	},
+	{
+		name: "Freezer",
+		life: 200,
+	},
+	{
+		name: "Cell",
+		life: 300,
+	},
+];
+
 function EnnemyCard() {
-	return <div className="card-container"></div>;
+	const [ennemyIndex, setEnnemyIndex] = useState(0);
+	const [ennemyLife, setEnnemyLife] = useState(ennemyList[ennemyIndex].life);
+
+	// Utilise useEffect pour réinitialiser la vie quand l'ennemi change
+	useEffect(() => {
+		setEnnemyLife(ennemyList[ennemyIndex].life);
+	}, [ennemyIndex]);
+
+	const handleClickAttack = () => {
+		if (ennemyLife > 1) {
+			setEnnemyLife(ennemyLife - 1);
+		} else {
+			alert(`Tu as battu ${ennemyList[ennemyIndex].name} !`);
+			setEnnemyIndex((prevIndex) => prevIndex + 1);
+		}
+	};
+
+	return (
+		<div className="card-container">
+			<img src="" alt="ennemy" width="390px" height="220px" />
+			<h1 className="ennemy-title">{ennemyList[ennemyIndex].name}</h1>
+			<p className="ennemy-pv">Point de Vie : {ennemyLife}</p>
+			<button
+				className="button-attack"
+				type="button"
+				onClick={handleClickAttack}
+			>
+				Attack !
+			</button>
+		</div>
+	);
 }
 
 export default EnnemyCard;
--- src/components/options/Option.tsx
+++ src/components/options/Option.tsx
@@ -1,22 +1,23 @@
 interface OptionProps {
-    label: string;
-    isAvailable: boolean;
-    onClick: () => void;
-    className: string;
+	label: string;
+	isAvailable: boolean;
+	onClick: () => void;
+	className: string;
 }
 
 const Option = ({ label, isAvailable, onClick, className }: OptionProps) => {
-    return (
-        <li>
-            <button
-                className={className}
-                onClick={onClick}
-                disabled={!isAvailable}
-            >
-                {label}
-            </button>
-        </li>
-    );
-}
+	return (
+		<li>
+			<button
+				type="button"
+				className={className}
+				onClick={onClick}
+				disabled={!isAvailable}
+			>
+				{label}
+			</button>
+		</li>
+	);
+};
 
 export default Option;
