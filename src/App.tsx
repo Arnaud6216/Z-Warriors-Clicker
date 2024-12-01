893c179f1be9bea087c2a6ee20a988799f0e30c2
Guevaer Arnaud <guevaer8@gmail.com>
2024-12-01 16:57:43 +0100

    passing the homepage into a component, add adding a router

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import EnnemyCard from "./components/Card/EnnemyCard";
 import Card from "./components/Card/PlayerCard";
 import Tech from "./components/Tech/Tech";
 import { Provider } from "./components/options/Context";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-	const [isGameStarted, setIsGameStarted] = useState(false);
+	const [currentLocation, setCurrentLocation] = useState("/");
 	const [isMusicPlaying, setIsMusicPlaying] = useState(false);
 
 	const musicList = ["src/assets/Gohan-anger.mp3", "src/assets/change.mp3"];
@@ -15,7 +16,7 @@ function App() {
 	const currentTrackIndex = useRef(0); //stocke une valeur mutable qui ne déclenche pas de rendu lorsqu'elle est modifiée
 
 	const handleStartGame = () => {
-		setIsGameStarted(true);
+		setCurrentLocation("/game")
 		playMusic(0);
 	};
 
@@ -50,33 +51,9 @@ function App() {
 
 	return (
 		<>
-			{!isGameStarted ? (
-				<div className="home-container">
-					<h1>Dragon Ball Z: Clicker Power</h1>
-					<p className="home-paragraph">
-						Bienvenue dans Dragon Ball Z: Clicker Power, un jeu où votre
-						objectif est de cliquer pour devenir plus puissant et vaincre des
-						ennemis de plus en plus forts !
-					</p>
-					<p className="home-paragraph">
-						Cliquez sur le <strong>bouton</strong> pour augmenter votre niveau
-						de puissance. Transformez vous en super saiyen, utilisez des
-						techniques comme le <strong>Kamehameha</strong> et la{" "}
-						<strong>Spirit Bomb</strong>, adaptez vous pour battre vos ennemis.
-					</p>
-					<p className="home-paragraph">
-						Cliquez sur le bouton "Commencer le jeu" pour débuter l'aventure.
-					</p>
-					<button
-						type="button"
-						className="start-button"
-						onClick={handleStartGame}
-					>
-						Commencer le jeu
-					</button>
-				</div>
-			) : (
-				<Provider>
+			
+			{currentLocation === "/" &&	<Homepage handleStartGame={handleStartGame}/>} 
+			{currentLocation === "/game" && <Provider>
 					<div>
 						<button className="musique" type="button" onClick={handleMusic}>
 							Musique On/Off
@@ -86,7 +63,7 @@ function App() {
 					<Tech />
 					<EnnemyCard />
 				</Provider>
-			)}
+			}
 		</>
 	);
 }
