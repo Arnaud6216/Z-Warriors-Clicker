994cc01b999a9a8bb8436a4585780d1624be5bd9
Guevaer Arnaud <guevaer8@gmail.com>
2024-12-31 16:53:27 +0100

    creating a registration page

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,18 @@ import Card from "./components/Card/PlayerCard";
 import Tech from "./components/Tech/Tech";
 import { Provider } from "./components/options/Context";
 import Homepage from "./components/Homepage/Homepage";
+import Registration from "./components/Registration/Registration";
 
 function App() {
+
+	const [isLogged, setIsLogged] = useState(false);
+
 	const [currentLocation, setCurrentLocation] = useState("/");
+
 	const [isMusicPlaying, setIsMusicPlaying] = useState(false);
-
 	const musicList = ["src/assets/music/Gohan-anger.mp3", "src/assets/music/change.mp3"];
-
 	const currentAudioRef = useRef<HTMLAudioElement | null>(null);
 	const currentTrackIndex = useRef(0); //stocke une valeur mutable qui ne déclenche pas de rendu lorsqu'elle est modifiée
-
-	const handleStartGame = () => {
-		setCurrentLocation("/game")
-		playMusic(0);
-	};
 
 	const playMusic = (index: number) => {
 		if (index >= musicList.length) {
@@ -49,21 +47,32 @@ function App() {
 		}
 	};
 
+	const handleStartGame = () => {
+		setCurrentLocation("/game")
+		playMusic(0);
+	};
+
+
+
 	return (
 		<>
-			
-			{currentLocation === "/" &&	<Homepage handleStartGame={handleStartGame}/>} 
-			{currentLocation === "/game" && <Provider>
-					<div>
-						<button className="musique" type="button" onClick={handleMusic}>
-							Musique On/Off
-						</button>
-					</div>
-					<Card />
-					<Tech />
-					<EnnemyCard />
-				</Provider>
-			}
+			{isLogged === false ? <Registration setIsLogged={setIsLogged}/> : (
+				<>
+					{currentLocation === "/" && <Homepage handleStartGame={handleStartGame} />}
+					{currentLocation === "/game" && (
+						<Provider>
+							<div>
+								<button className="musique" type="button" onClick={handleMusic}>
+									Musique On/Off
+								</button>
+							</div>
+							<Card />
+							<Tech />
+							<EnnemyCard />
+						</Provider>
+					)}
+				</>
+			)}
 		</>
 	);
 }
