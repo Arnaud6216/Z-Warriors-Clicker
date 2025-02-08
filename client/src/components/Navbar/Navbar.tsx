7789e12404a8de90045d50fb8899c6fc16d1e59e
Guevaer Arnaud <guevaer8@gmail.com>
2025-02-08 11:41:55 +0100

    updating disclaimer

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -40,17 +40,14 @@ function Navbar() {
     <>
       <header className="navbar-container">
         <p className="disclaimer-text">
-          <strong>Disclaimer :</strong> Ce jeu est un projet de fan créé à des
-          fins non commerciales et éducatives. Il s'inspire de l'univers de
-          Dragon Ball, qui est une propriété de Toei Animation, Akira Toriyama,
-          et leurs détenteurs de droits respectifs. Ce projet n'est en aucun cas
-          affilié, approuvé ou sponsorisé par les créateurs ou les détenteurs
-          des droits de Dragon Ball Z. Tous les droits sur les noms, personnages
-          et marques appartiennent à leurs propriétaires respectifs.
+          <strong>Disclaimer :</strong> Ce jeu est un projet de fan, non
+          commercial, inspiré de Dragon Ball. Il n'est en aucun cas affilié ou
+          approuvé par les créateurs ou détenteurs de droits de Dragon Ball Z.
+          Tous les droits appartiennent à leurs propriétaires respectifs.
         </p>
         <div className="progress-container">
           <p className="progress-username">{`Bienvenue ${progress?.username} !`}</p>
-          <p className="progress-ennemy">{`Dernier ennemi vaincu : ${progress?.name}`}</p>
+          <p className="progress-ennemy">{`Dernier ennemi affronté : ${progress?.name}`}</p>
         </div>
         <button className="logout-button" type="button" onClick={handleLogout}>
           Deconnexion
